28caa7b932c99ce7f340b6e490779742a4fc2893
Steve Lianoglou <slianoglou@gmail.com>
2010-08-13 21:04:18 -0700

    Adding methods to identify uniquely mappable regions of the genome

---
+++ R/constants.R
@@ -0,0 +1,10 @@
+.GFX <- list(
+  cache=list(
+    environment.key='GenomicFeaturesX_CACHE_DIR'
+  ),
+  uniqueness=list(
+    min.length=20,  ## Only have kmer lengths 20-54 as of 2010-03-11
+    max.length=54
+  )
+  
+)
---
+++ R/uniqueness.R
@@ -0,0 +1,180 @@
+withinUniquenessBounds <- function(k) {
+  .GFX$uniqueness$min.length <= k && k <= .GFX$uniqueness$max.length
+}
+
+
+##' Returns a logical vector that is the same length as ranges indicating
+##' whether that range is uniquelly mappable in the given genome
+##'
+##' @param ranges Either a \code{GRanges} object from which the chromosome(s)
+##' are inferred, or an \code{IRanges} object
+##' or an IRanges object,hg18, mm9, etc.
+##' @param genome hg18, mm9, etc.
+##' @param chromosome the name of the chromosome these ranges belong
+setGeneric("flagUniqueRanges",
+function(ranges, genome, chromosme=NULL, ...) {
+  standardGeneric("flagUniqueRanges")
+})
+
+
+## you can pass in a uniqueness.map to use one that's already been loaded.
+## if this is done and use.universal is TRUE, we assume it is the universall/all
+## map, otherwise we assume that the map is for the same width that the ranges
+## and all of the ranges must be the same width
+setMethod("flagUniqueRanges", c(ranges="IRanges"),
+function(ranges, genome, chromosome, use.unversal=FALSE,
+         uniqueness.map=NULL, ...) {
+  if (missing(genome)) {
+    stop("Need genome")
+  }
+  if (is.null(chromosome)) {
+    stop("Need chromosome")
+  }
+  
+  are.unique <- logical(length(ranges))  
+  widths <- unique(width(ranges))
+
+  if (!is.null(uniqueness.map)) {
+    if (!use.universal && length(width) > 1) {
+      warning("We can't use the map passed in, will load it on the fly")
+      uniqueness.map <- NULL
+    }
+  }
+
+  if (use.universal && is.null(uniqueness.map)) {
+    uniqueness.map <- getUniquenessMap(genome, chromosome, 'all')
+  }
+  
+  for (w in widths) {
+    these <- width(ranges) == w
+    starts <- start(ranges[these])
+    if (use.universal) {
+      if (withinUniquenessBounds(w)) {
+        is.unique <- as.logical(uniqueness.map[starts] <= w)
+      } else {
+        warning("Width of range (", w, ") is not within bounds, ",
+                "all ranges marked as FALSE")
+        is.unique <- logical(length(starts))
+      }
+    } else {
+      uniqueness.map <- tryCatch(getUniquenessMap(genome, chromosome, w),
+                                 error=function(e) NULL)
+      if (is.null(uniqueness.map)) {
+        warning("No unique map for ",
+                sprintf("%s.%s %d-mer", genome, chromosome, w),
+                " all ranges of this length are FALSE")
+      }
+      is.unique <- as.logical(uniqueness.map[starts] == 1L)
+    }
+    are.unique[these] <- is.unique
+  }
+  
+  are.unique
+})
+
+setMethod("flagUniqueRanges", c(ranges="GRanges"),
+function(ranges, genome, chromosome, use.universal=FALSE, ...) {
+  chromosomes <- unique(seqnames(ranges))
+  result <- logical(length(ranges))
+  
+  for (chr in chromosomes) {
+    these <- which(seqnames(ranges) == chr)
+    u <- flagUniqueRanges(ranges(ranges[these]), genome, chr, use.unversal)
+    result[these] <- u
+  }
+
+  result
+})
+
+################################################################################
+## Utility functions
+
+##' @param genome hg18, mm9, etc
+##' @param chromosome chr1, chrX, etc.
+##' @param k the length of the kmer to check, or 'all' to use the "universal"
+##' uniqueness map
+.uniquenessMapFilePath <- function(genome, chromosome, k, cache.dir=NULL) {
+  if (suppressWarnings(is.na(as.integer(k)))) {
+    if (!is.character(k) && (k != "all")) {
+      stop("k can only be an integer, or 'all'")
+    }
+  } else {
+    if (!withinUniquenessBounds(k)) {
+      stop("Uniqueness maps only exists for kmers >= ",
+           .GFX$uniqueness$min.length, " and <= ",
+           .GFX$uniqueness$max.length)
+    }
+    k <- sprintf("k%d", k)
+  }
+  
+  cache.dir <- GFXCacheDir(cache.dir, 'uniqueness', genome, k)
+  name <- paste(chromosome, "Rle.rda", sep=".")
+  file.path(cache.dir, name)
+}
+
+setGeneric("getUniquenessMap",
+function(x, chromosome, k, ...) {
+  standardGeneric("getUniquenessMap")
+})
+
+## x : genome
+setMethod("getUniquenessMap", c(x="character"),
+function(x, chromosome, k, ...) {
+  file.name <- .uniquenessMapFilePath(x, chromosome, k)
+  if (!file.exists(file.name)) {
+    stop("No uniqueness map available for genome ", x,
+         " and read length ", k)
+  }
+  load(file.name)
+  uniqueness
+})
+
+setMethod("getUniquenessMap", c(x="GenomicCache"),
+function(x, chromosome, k, ...) {
+  getUniquenessMap(genome(x), chromosome, k, allow.universal, ...)
+})
+
+## setGeneric("haveUniquenessMap",
+## function(x, chromosome, k, ...) {
+##   standardGeneric("haveUniquenessMap")
+## })
+##
+## setMethod("haveUniquenessMap", c(x="character"),
+## function(x, chromosome, k, ...) {
+##   file.name <- .uniquenessMapFilePath(x, chromosome, k, ...)
+##   file.exists(file.name)
+## })
+##
+## setMethod("haveUniquenessMap", c(x="GenomicCache"),
+## function(x, chromosome, k, ...) {
+##   haveUniquenessMap(genome(x), chromosome, k, ...)
+## })
+
+###############################################################################
+# Functions to convert Anshul's uniqueness maps to Rle objects
+# These functions aren't meant to be used by the general public
+###############################################################################
+convertUniqueness2Rle <- function(u.dir, genome='hg18') {
+  files <- dir(u.dir, pattern="uint8.unique\\.*")
+  gdb <- GenomeDB('hg18')
+  chromosomes <- sapply(strsplit(files, ".", fixed=TRUE), '[', 1)
+  
+  uniqueness <- lapply(seq_along(files), function(idx) {
+    file.name <- files[idx]
+    chr <- chromosomes[idx]
+    chr.length <- getChromosomeLength(gdb, chr)
+    cat("Chromosome", chr, "-- length:", chr.length, "\n")
+    # cat("  ", file.path(u.dir, file.name), "\n")
+    u <- readBin(file.path(u.dir, file.name), what='integer', size=1,
+                 signed=FALSE, n=chr.length+100)
+    if (length(u) != chr.length) {
+      cat("  Lengths don't match")
+    }
+    uniqueness <- Rle(u)
+    save(uniqueness, file=file.path(u.dir, paste(chr, "Rle.rda", sep=".")))
+    Rle(u)
+  })
+  names(uniqueness) <- chromosomes
+  
+  invisible(uniqueness)
+}
