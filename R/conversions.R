9c5f975ab8afa649a0bdc20b23e9d3213ece27ae
Steve Lianoglou <slianoglou@gmail.com>
2010-10-27 23:37:01 -0400

    Better methods to convert G/IRanges to<->from data.table objects

---
+++ R/conversions.R
@@ -0,0 +1,42 @@
+setOldClass(c("data.table", "data.frame"))
+
+setAs("GRanges", "data.table", function(from) {
+  if (length(from) == 0L) {
+    return(data.table)
+  }
+  data.table(as.data.frame(from))
+})
+
+setAs("data.table", "GRanges", function(from) {
+  if (nrow(from) == 0L) {
+    return(GRanges())
+  }
+  if (!all(c('seqnames', 'start', 'end') %in% colnames(from))) {
+    stop("seqnames, start, end required")
+  }
+  .strand <- if ('strand' %in% colnames(from)) from$strand else '*'
+  gr <- GRanges(seqnames=from$seqnames, ranges=IRanges(from$start, from$end),
+                strand=.strand)
+  gr.colnames <- c('seqnames', 'start', 'end', 'strand', 'width')
+  meta.cols <- colnames(from)[!colnames(from) %in% gr.colnames]
+  if (length(meta.cols) > 0) {
+    values(gr) <- DataFrame(from[, meta.cols, with=FALSE])
+  }
+  gr
+})
+
+setAs("data.table", "IRanges", function(from) {
+  if (nrow(from) == 0L) {
+    return(GRanges())
+  }
+  if (!all(c('start', 'end') %in% colnames(from))) {
+    stop("seqnames, start, end required")
+  }
+  ir <- IRanges(from$start, from$end)
+  ir.colnames <- c('start', 'end', 'width')
+  meta.cols <- colnames(from)[!colnames(from) %in% ir.colnames]
+  if (length(meta.cols) > 0) {
+    values(ir) <- DataFrame(from[, meta.cols, with=FALSE])
+  }
+  ir
+})
