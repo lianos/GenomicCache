0cc649f6a08e52e7c5573cfb3fcf82b79a925828
Steve Lianoglou <slianoglou@gmail.com>
2011-11-29 13:28:55 -0500

    Added codes to (rematch) seqinfo between a GRanges object and a 'standard' (from a BSgenome)

--- R/AnnotatedChromosome-utils.R
+++ R/AnnotatedChromosome-utils.R
@@ -1,5 +1,5 @@
 ##' Adds `intron.utr3` category for 3'UTRs "inside" the longest isofrom.
-##' 
+##'
 ##' This is a combination of a region being defined as `utr3` with a
 ##' utr3.index != 1
 annotateWithIntronUtr3 <- function(ag, ...) {
@@ -7,11 +7,11 @@ annotateWithIntronUtr3 <- function(ag, ...) {
   dt <- data.table(as.data.frame(ag),
                    key=c('seqnames', 'strand', 'entrez.id', 'start'))
   dt$exon.anno <- as.character(dt$exon.anno)
-  
+
   if (verbose) {
     cat("... annotating intron.utr3's\n")
   }
-  
+
   ag.dt <- dt[, {
     SD <- copy(.SD)
     if (!is.na(entrez.id) && any(utr3.index > 1)) {
@@ -20,13 +20,13 @@ annotateWithIntronUtr3 <- function(ag, ...) {
     }
     SD
   }, by=head(key(dt), -1)]
-  
+
   ag <- as(ag.dt, 'GRanges')
   values(ag) <- DataFrame(lapply(values(ag), function(x) {
     if (is.factor(x)) x <- as.character(x)
     x
   }))
-  
+
   class(ag) <- 'AnnotatedChromosome'
   ag
 }
@@ -42,3 +42,16 @@ extractSeqinfo <- function(x, bsg) {
   si.bsg[seqlevels(si.bsg)[seqlevels(si.bsg) %in% seqlevels(x)]]
 }
 
+rematchSeqinfo <- function(x, bsg) {
+  if (inherits(try(seqinfo(x)), "try-error")) {
+    stop("An object with `seqinfo` is required for `x`")
+  }
+  if (!inherits(bsg, 'BSgenome')) {
+    stop("`bsg` must be a BSgenome object")
+  }
+  si.bsg <- seqinfo(bsg)
+  si <- si.bsg[seqlevels(si.bsg)[seqlevels(si.bsg) %in% seqlevels(x)]]
+  new2old <- match(seqlevels(si), seqlevels(x))
+  seqinfo(x, new2old) <- si
+  x
+}
